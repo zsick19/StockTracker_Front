ae342b645011635076a4c1708a571a9ad0de9c81
zsick19 <39588754+zsick19@users.noreply.github.com>
2025-12-30 13:25:30 -0700

    Watchlist can now be created, retitled and deleted. Tickers can be added and removed from watchlists. Styles updated with a rename form and confirm delete toggle.

--- src/features/WatchList/WatchListSliceApi.js
+++ src/features/WatchList/WatchListSliceApi.js
@@ -5,69 +5,127 @@ export const WatchListApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     createUserWatchList: builder.mutation({
       query: (args) => ({
-        url: `/user/${args.userId}/watchlist?macro=${args.macroWatchlist}`,
+        url: `/user/watchlist/${args.userId}?macro=${args.macroWatchlist}`,
         method: "POST",
         body: { ...args.watchListInfo },
       }),
-      async onQueryStarted(args, { dispatch, queryFulfilled }) {
-        try {
+      async onQueryStarted(args, { dispatch, queryFulfilled })
+      {
+        try
+        {
           const { data: createdPost } = await queryFulfilled;
           dispatch(
             InitializationApiSlice.util.updateQueryData(
               "getUserInitialization",
               { userId: args.userId },
-              (draft) => {
+              (draft) =>
+              {
                 draft.macroWatchLists.push(createdPost);
               }
             )
           );
-        } catch {
+        } catch
+        {
+          // If mutation fails, the cache remains untouched
+        }
+      },
+    }),
+    updateWatchListName: builder.mutation({
+      query: (args) => ({
+        url: `/user/watchlist/${args.watchlistId}/edit?updatedTitle=${args.updatedTitle}`,
+        method: 'PUT'
+      }),
+      async onQueryStarted(args, { dispatch, queryFulfilled })
+      {
+        try
+        {
+          const { data: updatedTitle } = await queryFulfilled;
+          dispatch(InitializationApiSlice.util.updateQueryData("getUserInitialization", { userId: "6952bd331482f8927092ddcc" },
+            (draft) => { draft.macroWatchLists.map((watchlist) => { if (watchlist._id === args.watchlistId) { watchlist.title = updatedTitle.updatedTitle } }); })
+          );
+        } catch
+        {
+          // If mutation fails, the cache remains untouched
+        }
+      },
+    }),
+    deleteUserWatchList: builder.mutation({
+      query: (args) => ({
+        url: `/user/watchlist/${args.watchlistId}/edit`,
+        method: "DELETE",
+      }),
+      async onQueryStarted(args, { dispatch, queryFulfilled })
+      {
+        try
+        {
+          const { data: deletedWatchList } = await queryFulfilled;
+          dispatch(
+            InitializationApiSlice.util.updateQueryData(
+              "getUserInitialization",
+              { userId: "6952bd331482f8927092ddcc" },
+              (draft) => { draft.macroWatchLists = draft.macroWatchLists.filter((watchlist) => { return watchlist._id !== deletedWatchList.deletedWatchlist }); }
+            )
+          );
+        } catch
+        {
           // If mutation fails, the cache remains untouched
         }
       },
     }),
     addTickerToWatchList: builder.mutation({
       query: (args) => ({
-        url: `/user/watchlist/${args.watchlistId}?ticker=${args.tickerToAdd}`,
+        url: `/user/watchlist/${args.watchlistId}/tickers?ticker=${args.tickerToAdd}`,
         method: "POST",
       }),
-      async onQueryStarted(args, { dispatch, queryFulfilled }) {
-        try {
+      async onQueryStarted(args, { dispatch, queryFulfilled })
+      {
+        try
+        {
           const { data: addedWatchListTicker } = await queryFulfilled;
           dispatch(
             InitializationApiSlice.util.updateQueryData(
               "getUserInitialization",
               { userId: "6952bd331482f8927092ddcc" },
-              (draft) => {
-                draft.macroWatchLists.map((watchlist) => {
-                  if (watchlist._id === args.watchlistId) {
+              (draft) =>
+              {
+                draft.macroWatchLists.map((watchlist) =>
+                {
+                  if (watchlist._id === args.watchlistId)
+                  {
                     watchlist.tickersContained.push(addedWatchListTicker);
                   }
                 });
               }
             )
           );
-        } catch {
+        } catch
+        {
           // If mutation fails, the cache remains untouched
         }
       },
     }),
     removeTickerFromWatchList: builder.mutation({
       query: (args) => ({
-        url: `/user/watchlist/${args.watchlistId}?ticker=${args.tickerToRemove}`,
+        url: `/user/watchlist/${args.watchlistId}/tickers?ticker=${args.tickerToRemove}`,
         method: "PUT",
       }),
-      async onQueryStarted(args, { dispatch, queryFulfilled }) {
-        try {
+      async onQueryStarted(args, { dispatch, queryFulfilled })
+      {
+        try
+        {
           const { data: removedWatchListTicker } = await queryFulfilled;
           dispatch(
             InitializationApiSlice.util.updateQueryData(
               "getUserInitialization",
               { userId: "6952bd331482f8927092ddcc" },
-              (draft) => {
-                draft.macroWatchLists.map((watchlist) => {
-                  if (watchlist._id === args.watchlistId) {
-                    watchlist.tickersContained=watchlist.tickersContained.filter((t) => {
+              (draft) =>
+              {
+                draft.macroWatchLists.map((watchlist) =>
+                {
+                  if (watchlist._id === args.watchlistId)
+                  {
+                    watchlist.tickersContained = watchlist.tickersContained.filter((t) =>
+                    {
                       return t.ticker !== removedWatchListTicker.tickerRemoved;
                     });
                   }
@@ -75,7 +133,8 @@ export const WatchListApiSlice = apiSlice.injectEndpoints({
               }
             )
           );
-        } catch {
+        } catch
+        {
           // If mutation fails, the cache remains untouched
         }
       },
@@ -85,6 +144,8 @@ export const WatchListApiSlice = apiSlice.injectEndpoints({
 
 export const {
   useCreateUserWatchListMutation,
+  useUpdateWatchListNameMutation,
+  useDeleteUserWatchListMutation,
   useAddTickerToWatchListMutation,
   useRemoveTickerFromWatchListMutation,
 } = WatchListApiSlice;
